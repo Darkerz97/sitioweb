ac7b4dfc50e88107d2663f9c8e4bbd6a935513bb
Darkerz97 <122547460+Darkerz97@users.noreply.github.com>
2025-09-12 09:02:41 -0600

    se agrega profile en el dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,8 +3,11 @@
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
             {{ __('Dashboard') }}
         </h2>
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('profile') }}
+        </h2>
     </x-slot>
-    
+
     <div class="relative inline-block text-left">
     <button id="menu-button" aria-expanded="true" aria-haspopup="true"
         class="inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none"
