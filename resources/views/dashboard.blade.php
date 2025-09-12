9f1b3255038cfa5ab1d4c31145630774ebffd4d0
Darkerz97 <darkers2010@gmail.com>
2025-09-12 10:42:28 -0600

    e eliminan elementos del menu que no funcionaron

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,22 +3,8 @@
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
             {{ __('Dashboard') }}
         </h2>
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('profile') }}
-        </h2>
     </x-slot>
 
-    <div class="relative inline-block text-left">
-    <button id="menu-button" aria-expanded="true" aria-haspopup="true"
-        class="inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none"
-        onclick="toggleMenu()">
-        Menú
-        <svg class="-mr-1 ml-2 h-5 w-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-            stroke="currentColor" aria-hidden="true">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                d="M19 9l-7 7-7-7" />
-        </svg>
-    </button>
 
     <div id="menu" class="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 hidden"
         role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabindex="-1">
