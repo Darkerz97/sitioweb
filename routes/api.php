ec526f1ebfaf7d6378f3e40bc05b12eb26ffd7e4
Darkerz97 <darkers2010@gmail.com>
2025-09-26 22:37:21 -0600

    se modifican las rutas de las api

--- routes/api.php
+++ routes/api.php
@@ -1,15 +1,23 @@
-
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\CustomerController;
 use App\Http\Controllers\StoreController;
+use App\Http\Controllers\SkuController;
+use App\Http\Controllers\SaleController;
+use App\Http\Controllers\PaymentController;
+use App\Http\Controllers\TournamentController;
+use App\Http\Controllers\TournamentPlayerController;
 
-// Rutas API para Card Bastion
+// API Routes for Card Bastion
 Route::middleware('api')->group(function () {
     Route::apiResource('products', ProductController::class);
     Route::apiResource('customers', CustomerController::class);
     Route::apiResource('stores', StoreController::class);
+    Route::apiResource('skus', SkuController::class);
+    Route::apiResource('sales', SaleController::class);
+    Route::apiResource('payments', PaymentController::class);
+    Route::apiResource('tournaments', TournamentController::class);
+    Route::apiResource('tournament-players', TournamentPlayerController::class);
 });
